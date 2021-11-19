4024557e4a28af981b2a0623e073576e5e1f534c
Hamed Al Sarmi <alsarmih@cardiff.ac.uk>
2021-11-19 10:08:44 +0000

    Added information table

--- cemetery_database.sql
+++ cemetery_database.sql
@@ -1,4 +1,5 @@
 DROP TABLE IF EXISTS Login;
+DROP TABLE IF EXISTS Information;
 
 CREATE TABLE IF NOT EXISTS `Login` (
   `ID`		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
@@ -7,3 +8,16 @@ CREATE TABLE IF NOT EXISTS `Login` (
 );
 
 INSERT INTO 'Login'('Username','Password') VALUES ('admin','admin');
+
+CREATE TABLE IF NOT EXISTS `Information` (
+  `ID`		INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+  `Name`	TEXT NOT NULL,
+  `Date of birth`	TEXT NOT NULL,
+  `Date of death`	TEXT NOT NULL,
+  `Information`	TEXT NOT NULL
+  `Cemetery section`	TEXT NOT NULL
+  `Grave number`	TEXT NOT NULL
+  `Image`	TEXT NOT NULL
+);
+
+INSERT INTO 'Inforamtion'('Name', 'Date of birth', 'Date of death', 'Information', 'Latitude', 'Longitude', 'Image') VALUES ('', '', '', '', '')
